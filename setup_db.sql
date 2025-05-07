fe5b786c8631539136424fa0bc11d4d1f1ed85e6
Mauro <maupereyra96@gmail.com>
2025-05-07 19:36:25 -0300

    feat: Initialize docker settings

--- setup_db.sql
+++ setup_db.sql
@@ -1,42 +1,104 @@
-DROP DATABASE IF EXISTS empleados;
-CREATE DATABASE empleados CHARACTER SET utf8mb4;
-USE empleados;
+DROP DATABASE IF EXISTS jardineria;
+CREATE DATABASE jardineria CHARACTER SET utf8mb4;
+USE jardineria;
 
-CREATE TABLE departamento (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL,
-  presupuesto DOUBLE UNSIGNED NOT NULL,
-  gastos DOUBLE UNSIGNED NOT NULL
+CREATE TABLE oficina (
+  codigo_oficina VARCHAR(10) NOT NULL,
+  ciudad VARCHAR(30) NOT NULL,
+  pais VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (codigo_oficina)
 );
 
 CREATE TABLE empleado (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nif VARCHAR(9) NOT NULL UNIQUE,
-  nombre VARCHAR(100) NOT NULL,
-  apellido1 VARCHAR(100) NOT NULL,
-  apellido2 VARCHAR(100),
-  id_departamento INT UNSIGNED,
-  FOREIGN KEY (id_departamento) REFERENCES departamento(id)
+  codigo_empleado INTEGER NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  apellido1 VARCHAR(50) NOT NULL,
+  apellido2 VARCHAR(50) DEFAULT NULL,
+  extension VARCHAR(10) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  codigo_oficina VARCHAR(10) NOT NULL,
+  codigo_jefe INTEGER DEFAULT NULL,
+  puesto VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (codigo_empleado),
+  FOREIGN KEY (codigo_oficina) REFERENCES oficina (codigo_oficina),
+  FOREIGN KEY (codigo_jefe) REFERENCES empleado (codigo_empleado)
 );
 
-INSERT INTO departamento VALUES(1, 'Desarrollo', 120000, 6000);
-INSERT INTO departamento VALUES(2, 'Sistemas', 150000, 21000);
-INSERT INTO departamento VALUES(3, 'Recursos Humanos', 280000, 25000);
-INSERT INTO departamento VALUES(4, 'Contabilidad', 110000, 3000);
-INSERT INTO departamento VALUES(5, 'I+D', 375000, 380000);
-INSERT INTO departamento VALUES(6, 'Proyectos', 0, 0);
-INSERT INTO departamento VALUES(7, 'Publicidad', 0, 1000);
+CREATE TABLE gama_producto (
+  gama VARCHAR(50) NOT NULL,
+  descripcion_texto TEXT,
+  descripcion_html TEXT,
+  imagen VARCHAR(256),
+  PRIMARY KEY (gama)
+);
 
-INSERT INTO empleado VALUES(1, '32481596F', 'Aarón', 'Rivero', 'Gómez', 1);
-INSERT INTO empleado VALUES(2, 'Y5575632D', 'Adela', 'Salas', 'Díaz', 2);
-INSERT INTO empleado VALUES(3, 'R6970642B', 'Adolfo', 'Rubio', 'Flores', 3);
-INSERT INTO empleado VALUES(4, '77705545E', 'Adrián', 'Suárez', NULL, 4);
-INSERT INTO empleado VALUES(5, '17087203C', 'Marcos', 'Loyola', 'Méndez', 5);
-INSERT INTO empleado VALUES(6, '38382980M', 'María', 'Santana', 'Moreno', 1);
-INSERT INTO empleado VALUES(7, '80576669X', 'Pilar', 'Ruiz', NULL, 2);
-INSERT INTO empleado VALUES(8, '71651431Z', 'Pepe', 'Ruiz', 'Santana', 3);
-INSERT INTO empleado VALUES(9, '56399183D', 'Juan', 'Gómez', 'López', 2);
-INSERT INTO empleado VALUES(10, '46384486H', 'Diego','Flores', 'Salas', 5);
-INSERT INTO empleado VALUES(11, '67389283A', 'Marta','Herrera', 'Gil', 1);
-INSERT INTO empleado VALUES(12, '41234836R', 'Irene','Salas', 'Flores', NULL);
-INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero', NULL);
+CREATE TABLE cliente (
+  codigo_cliente INTEGER NOT NULL,
+  nombre_cliente VARCHAR(50) NOT NULL,
+  nombre_contacto VARCHAR(30) DEFAULT NULL,
+  apellido_contacto VARCHAR(30) DEFAULT NULL,
+  telefono VARCHAR(15) NOT NULL,
+  fax VARCHAR(15) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  ciudad VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  pais VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) DEFAULT NULL,
+  codigo_empleado_rep_ventas INTEGER DEFAULT NULL,
+  limite_credito NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_cliente),
+  FOREIGN KEY (codigo_empleado_rep_ventas) REFERENCES empleado (codigo_empleado)
+);
+
+CREATE TABLE pedido (
+  codigo_pedido INTEGER NOT NULL,
+  fecha_pedido date NOT NULL,
+  fecha_esperada date NOT NULL,
+  fecha_entrega date DEFAULT NULL,
+  estado VARCHAR(15) NOT NULL,
+  comentarios TEXT,
+  codigo_cliente INTEGER NOT NULL,
+  PRIMARY KEY (codigo_pedido),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente)
+);
+
+CREATE TABLE producto (
+  codigo_producto VARCHAR(15) NOT NULL,
+  nombre VARCHAR(70) NOT NULL,
+  gama VARCHAR(50) NOT NULL,
+  dimensiones VARCHAR(25) NULL,
+  proveedor VARCHAR(50) DEFAULT NULL,
+  descripcion text NULL,
+  cantidad_en_stock SMALLINT NOT NULL,
+  precio_venta NUMERIC(15,2) NOT NULL,
+  precio_proveedor NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_producto),
+  FOREIGN KEY (gama) REFERENCES gama_producto (gama)
+);
+
+CREATE TABLE detalle_pedido (
+  codigo_pedido INTEGER NOT NULL,
+  codigo_producto VARCHAR(15) NOT NULL,
+  cantidad INTEGER NOT NULL,
+  precio_unidad NUMERIC(15,2) NOT NULL,
+  numero_linea SMALLINT NOT NULL,
+  PRIMARY KEY (codigo_pedido, codigo_producto),
+  FOREIGN KEY (codigo_pedido) REFERENCES pedido (codigo_pedido),
+  FOREIGN KEY (codigo_producto) REFERENCES producto (codigo_producto)
+);
+
+CREATE TABLE pago (
+  codigo_cliente INTEGER NOT NULL,
+  forma_pago VARCHAR(40) NOT NULL,
+  id_transaccion VARCHAR(50) NOT NULL,
+  fecha_pago date NOT NULL,
+  total NUMERIC(15,2) NOT NULL,
+  PRIMARY KEY (codigo_cliente, id_transaccion),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente)
+);
