40aace57feecdaa1816ccb9cb05b6b20274f77cb
Mauro <maupereyra96@gmail.com>
2025-06-08 23:39:11 -0300

    Resolve 1 new exercise

--- exercises/one_table/ex_8.sql
+++ exercises/one_table/ex_8.sql
@@ -1,1 +1,17 @@
--- 
+--     Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
+
+--   Utilizando la función YEAR de MySQL.
+--   Utilizando la función DATE_FORMAT de MySQL.
+--   Sin utilizar ninguna de las funciones anteriores.
+
+SELECT DISTINCT codigo_cliente
+FROM pago
+WHERE YEAR(fecha_pago) = 2008
+
+SELECT DISTINCT codigo_cliente
+FROM pago
+WHERE DATE_FORMAT(fecha_pago,"%Y") = '2008'
+
+SELECT DISTINCT codigo_cliente
+FROM pago
+WHERE fecha_pago >= '2008-01-01' AND fecha_pago < '2009-01-01';
