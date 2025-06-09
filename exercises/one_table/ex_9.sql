0bfd49927b572e83ae77d3136e9fd57afd42dc22
Mauro <maupereyra96@gmail.com>
2025-06-09 18:18:18 -0300

    feat: Resolve 1 new exercise

--- exercises/one_table/ex_9.sql
+++ exercises/one_table/ex_9.sql
@@ -1,1 +1,5 @@
--- 
+-- Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo.
+
+SELECT codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega, estado
+FROM pedido
+WHERE fecha_entrega > fecha_esperada OR fecha_entrega IS NULL
